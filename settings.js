26109182790516571162aa902990e9ce9ec29a63
Patrick star <108621275+Patrickstarrrrrrrr@users.noreply.github.com>
2022-09-06 09:00:48 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,14 +22,14 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6281536907492','6281536907492','6281536907492'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6281536907492'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281536907492' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'Dian //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Patrick-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'Dian' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
@@ -52,7 +52,7 @@ global.sessionName = 'session'
 //terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
 global.limitawal = {
 premium: "Infinity",
-free: 15,
+free: 100,
 }
 
 //—————「 Set Image 」—————//
